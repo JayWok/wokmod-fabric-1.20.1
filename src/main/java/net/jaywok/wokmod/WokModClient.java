5d44dd538c76681eedf8db937e2f946a7a49f175
JayWok <jaydennlocke@gmail.com>
2023-08-11 01:50:34 -0400

    Initial commit

---
+++ src/main/java/net/jaywok/wokmod/WokModClient.java
@@ -0,0 +1,10 @@
+package net.jaywok.wokmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WokModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jaywok/wokmod/WokModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jaywok.wokmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WokModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
